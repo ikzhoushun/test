3d3fa3c2a4e8b0639bd9ef477f7b503b74c7e34a
ikzhoushun <1837116435@qq.com>
2023-04-09 19:50:02 +0800

    第二次提交

--- src/main/java/com/example/demo/HelloApplication.java
+++ src/main/java/com/example/demo/HelloApplication.java
@@ -1,9 +1,13 @@
 package com.example.demo;
 
+import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
+
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
 
 @ApplicationPath("/api")
 public class HelloApplication extends Application {
+    public static void main(String[] args) {
 
+    }
 }
