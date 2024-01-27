39a2eb85caa6c3ed0a20c186dda44899caabf64d
ikzhoushun <1837116435@qq.com>
2024-01-27 23:03:17 +0800

    测试

--- src/main/java/com/example/demo/HelloResource.java
+++ src/main/java/com/example/demo/HelloResource.java
@@ -1,13 +1,18 @@
 package com.example.demo;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 
-@Path("/hello-world")
+@Controller
 public class HelloResource {
-    @GET
-    @Produces("text/plain")
+    @RequestMapping("/test")
+    @ResponseBody
     public String hello() {
         return "Hello, World!";
     }
